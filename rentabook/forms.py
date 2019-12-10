b7cc757522436107481275d980207d1d66c904fe
meemeee <lethitramy27@gmail.com>
2019-12-10 18:51:07 +0900

    Add Edit book page

--- rentabook/forms.py
+++ rentabook/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+import datetime
 from django.contrib.auth.models import User
+from .views import BookInstance
 
 
 class RegisterForm(UserCreationForm):
@@ -13,3 +16,25 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+
+
+class EditBookForm(ModelForm):
+    def clean_due_back(self):
+       data = self.cleaned_data['due_back']
+       
+       # Check if a date is not in the past.
+       if data < datetime.date.today():
+           raise ValidationError(_('Invalid date - renewal in past'))
+
+       # Check if a date is in the allowed range (+4 weeks from today).
+       if data > datetime.date.today() + datetime.timedelta(weeks=4):
+           raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
+
+       # Remember to always return the cleaned data.
+       return data
+
+    class Meta:
+        model = BookInstance
+        fields = ['status', 'due_back']
+        labels = {'due_back': _('Renewal date')}
+        help_texts = {'due_back': _('Enter a date between now and 4 weeks (default 3).')} 
--- rentabook/urls.py
+++ rentabook/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookInstanceListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookInstanceDetailView.as_view(), name='book-detail'),
-    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('mybooks/', views.BooksByUserListView.as_view(), name='my-books'),
+    path('book/<int:pk>/edit/', views.edit_book, name='edit-book'),
 ]
 
 #Add Django site authentication urls (for login, logout, register)
